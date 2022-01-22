920d02b3b5c56f2c07bbdb51f1d34d5493990632
Ville <ruusvil@gmail.com>
2022-01-22 12:45:12 +0200

    added dynamic theme color based on selected nation

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ const App = () => {
     event.target.value !== "null" 
       ? setSelectedNation(event.target.value)
       : setSelectedNation(null)
+    handleColorThemeChange(event.target.value)
+  }
+
+  const handleColorThemeChange = (nation) => {
+    let faction = data.find(n => n.name === nation)
+    console.log(faction)
+    let color = faction.themeColor
+    let darkColor = faction.themeColorDark
+    let root = document.documentElement
+    root.style.setProperty('--nation-clr', color)
+    root.style.setProperty('--nation-clr-dark', darkColor)
+    console.log(color, darkColor)
   }
 
   const handleMaxPointChange = (event) => {
